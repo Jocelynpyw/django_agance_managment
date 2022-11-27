181cfb72af46b6ae9644fb4866c3b574b9b396ff
jocelyn pyw <jocelynwotcheu2@gmail.com>
2022-11-27 23:17:35 +0100

    my first commit

---
+++ Agence/urls.py
@@ -0,0 +1,9 @@
+from django import views
+from django.urls import path,include
+from .views import*
+
+urlpatterns = [
+    path('',home,name="home"),
+    path('login',login,name="login"),
+    path('register',register,name="register"),
+]
